211670aa8b09712aeefec66373acd6a614163b8b
Rory Dungan <rory.dungan@realseriousgames.com>
2015-12-18 11:44:00 +1000

    Added OVR Plugin to Unity project and started setting up resources for example

---
+++ UnityProject/Assets/Rotate.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+    public enum axis { X, Y, Z };
+
+    public axis Axis = axis.X;
+
+    // Use this for initialization
+    void Start () {
+    
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        Vector3 rotateAround = Vector3.zero;
+        switch (Axis) {
+            case axis.X:
+                rotateAround = Vector3.right;
+                break;
+            case axis.Y:
+                rotateAround = Vector3.up;
+                break;
+            case axis.Z:
+                rotateAround = Vector3.forward;
+                break;
+        }
+        this.transform.Rotate(rotateAround, 50 * Time.deltaTime);
+    }
+}
